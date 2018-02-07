3017bb27ec2b1d189d8a683b49cdd28ebf3a0ce1
= <orjichidi95@gmail.com>
2018-02-07 19:00:43 +0400

    update search template

--- choralcentral/settings/base.py
+++ choralcentral/settings/base.py
@@ -35,7 +35,6 @@ INTERNAL_IPS = ('127.0.0.1', 'localhost')
 FIXTURE_DIRS = [os.path.join(BASE_DIR, "fixtures")]
 
 SITE_ID = 1
-CART_SESSION_ID = "cart"
 
 LOGIN_REDIRECT_URL = reverse_lazy('song:index')
 LOGIN_URL = reverse_lazy('siteuser:login')
