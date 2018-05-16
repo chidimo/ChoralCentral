cb4e3805fc02e9c58dfdbe968ecd31b085560024
orji chidi <orjichidi95@gmail.com>
2018-05-16 08:58:24 +0000

    fb, gg, and tw backends okay

--- siteuser/save_social.py
+++ siteuser/save_social.py
@@ -13,6 +13,13 @@ from .models import SiteUser
 
 CustomUser = get_user_model()
 
+login_backends = {}
+login_backends['django'] = 'django.contrib.auth.backends.ModelBackend'
+login_backends['twitter'] = 'social_core.backends.twitter.TwitterOAuth'
+login_backends['google_oauth2'] = 'social_core.backends.google.GoogleOAuth2'
+login_backends['facebook'] = 'social_core.backends.facebook.FacebookOAuth2'
+login_backends['yahoo'] = 'social_core.backends.yahoo.YahooOAuth2'
+
 def save_avatar(image_url, model_object):
     response = requests.get(image_url)
     if response.status_code == 200:
@@ -21,29 +28,29 @@ def save_avatar(image_url, model_object):
 
 def save_social_profile(backend, user, response, *args, **kwargs):
     request = kwargs['request']
-    login_backend = 'django.contrib.auth.backends.ModelBackend',
-    if backend.name == "twitter":
-        with open("response-twitter.json", "w+") as fh:
-            json.dump(response, fh)
 
-        login_backend = 'social_core.backends.twitter.TwitterOAuth'
+    if backend.name == "twitter":
+        # with open("response-twitter.json", "w+") as fh:
+        #     json.dump(response, fh)
         screen_name = response['screen_name'] # twitter response contains a screen_name
         image = response['profile_image_url']
         location = response['location']
         name = response['name'].split()
         first_name = name[0]
         last_name = name[1]
-        email = "{}@twitter.com".format(screen_name) # make a fake email to satisfy create_user
+        email = response.get('email', None)
+        if email is None:
+            msg = """It appears you have no email set in your twitter account.
+            We have created a dummy email {} for your for purpose of registration.
+            Please be sure to change it to a real email.""".format(email)
+            messages.success(request, msg)
 
         if CustomUser.objects.filter(email=email).exists():
-            user = user = CustomUser.objects.get(email=email)
+            user = CustomUser.objects.get(email=email)
         else:
             user = CustomUser.objects.create_user(email=email, password=None)
             user.is_active = True
             user.save()
-            msg = "We have created a fake email {} for your for purpose of registration. \
-            Please be sure to edit it and add a real email".format(email)
-            messages.success(request, msg)
         try:
             su = SiteUser.objects.get(user__email=email)
         except ObjectDoesNotExist:
@@ -51,14 +58,12 @@ def save_social_profile(backend, user, response, *args, **kwargs):
                 user=user, screen_name=screen_name, first_name=first_name, last_name=last_name,
                 location=location)
             save_avatar(image, su)
-        login(request, user, backend=login_backend)
+        login(request, user, backend=login_backends['django'])
         return {'username' : screen_name}
 
     elif backend.name == 'google-oauth2':
         # with open("response-google.json", "w+") as fh:
         #     json.dump(response, fh)
-
-        login_backend = 'social_core.backends.google.GoogleOAuth2'
         screen_name = response['displayName'].strip()
         email = response['emails'][0]['value']
         first_name = response['name']['givenName']
@@ -66,7 +71,7 @@ def save_social_profile(backend, user, response, *args, **kwargs):
         image = response['image']['url'].split('?')[0]
 
         if CustomUser.objects.filter(email=email).exists():
-            user = user = CustomUser.objects.get(email=email)
+            user = CustomUser.objects.get(email=email)
         else:
             user = CustomUser.objects.create_user(email=email, password=None)
             user.is_active = True
@@ -77,36 +82,44 @@ def save_social_profile(backend, user, response, *args, **kwargs):
             su = SiteUser.objects.create(
                 user=user, screen_name=screen_name, first_name=first_name, last_name=last_name)
             save_avatar(image, su)
-        login(request, user, backend=login_backend)
+        login(request, user, backend=login_backends['django'])
         return {'username' : screen_name}
 
     elif backend.name == 'facebook':
-        with open("response-facebook.json", "w+") as fh:
-            json.dump(response, fh)
+        # with open("response-facebook.json", "w+") as fh:
+        #     json.dump(response, fh)
         name = response['name'].split()
         first_name = name[0]
         last_name = name[1]
+        screen_name = "{}-{}".format(first_name, last_name)
         email = response.get('email', None)
+        image = 'https://graph.facebook.com/{}/picture?type=large'.format(response['id'])
 
         if CustomUser.objects.filter(email=email).exists():
-            user = user = CustomUser.objects.get(email=email)
+            user = CustomUser.objects.get(email=email)
         else:
             user = CustomUser.objects.create_user(email=email, password=None)
             user.is_active = True
             user.save()
+        try:
+            su = SiteUser.objects.get(user__email=email)
+        except ObjectDoesNotExist:
+            su = SiteUser.objects.create(
+                user=user, screen_name=screen_name, first_name=first_name, last_name=last_name)
+            save_avatar(image, su)
+        login(request, user, backend=login_backends['django'])
+        return {'username' : screen_name}
 
     elif backend.name == 'yahoo-oauth2':
-        with open("response-yahoo.json", "w+") as fh:
-            json.dump(response, fh)
-
-        login_backend = 'social_core.backends.yahoo.YahooOAuth2'
+        # with open("response-yahoo.json", "w+") as fh:
+        #     json.dump(response, fh)
         image = response['image']['imageUrl']
         screen_name = response['nickname'] # not unique. check for collisions
 
         if SiteUser.objects.filter(screen_name=screen_name).exists():
             siteuser = SiteUser.objects.get(screen_name=screen_name)
             user = siteuser.user
-            login(request, user, backend=login_backend)
+            login(request, user, backend=login_backends['django'])
             return {'username' : screen_name}
 
         email = "ab@yahoo.com"
@@ -118,5 +131,5 @@ def save_social_profile(backend, user, response, *args, **kwargs):
             user=user, screen_name=screen_name, first_name='first_name', last_name='last_name')
         save_avatar(image, su)
 
-        login(request, user, backend=login_backend)
+        login(request, user, backend=login_backends['django'])
         return {'username' : screen_name}
