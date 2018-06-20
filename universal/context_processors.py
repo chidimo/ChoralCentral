b59908f138956a55f69a803d2f7dbead305778d8
immensity <orjichidi95@gmail.com>
2018-06-20 16:13:04 +0400

    remove user count

--- universal/context_processors.py
+++ universal/context_processors.py
@@ -5,7 +5,6 @@ def site_stats(request):
     return {
         'lang_count' : Language.objects.count(),
         'voice_count' : Voicing.objects.count(),
-        'user_count' : SiteUser.objects.count() - 1,
         'song_count' : Song.objects.filter(publish=True).count(),
     }
 
