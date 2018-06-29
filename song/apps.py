73dea84d3b2d4133e5ad9bd2773b7d7e7894f5cc
immensity <orjichidi95@gmail.com>
2018-06-29 20:10:54 +0400

    minor change

--- song/apps.py
+++ song/apps.py
@@ -1,7 +1,17 @@
 from django.apps import AppConfig
 import algoliasearch_django as algoliasearch
 
+from django.conf import settings
+
 from .index import SongIndex
+
+DJANGO_SETTINGS_MODULE = getattr(settings, 'DJANGO_SETTINGS_MODULE', None)
+
+import os
+
+print(os.environ['DJANGO_SETTINGS_MODULE'])
+
+print('SETTINGS: ', DJANGO_SETTINGS_MODULE)
 
 class SongConfig(AppConfig):
     name = 'song'
