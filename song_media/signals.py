67e7e2e11047f970ce6eb6da74c1368a4fba0385
immensity <orjichidi95@gmail.com>
2018-06-30 00:28:46 +0400

    deactivate song_media signals

--- song_media/signals.py
+++ song_media/signals.py
@@ -1,22 +1,22 @@
 
-import os
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# import os
+# from django.conf import settings
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
-from .models import Score, Midi
+# from .models import Score, Midi
 
-@receiver(post_save, sender=Score)
-def backup_score_on_upload(sender, instance, **kwargs):
-    """Backup scores"""
+# @receiver(post_save, sender=Score)
+# def backup_score_on_upload(sender, instance, **kwargs):
+#     """Backup scores"""
 
-    if os.environ['DJANGO_SETTINGS_MODULE'] == 'choralcentral.settings.prod':
-        os.system('source virtualenvwrapper.sh && workon __cc && python manage.py cc_backup_score')
-        # os.system('python manage.py cc_backup_score')
+#     if os.environ['DJANGO_SETTINGS_MODULE'] == 'choralcentral.settings.prod':
+#         os.system('source virtualenvwrapper.sh && workon __cc && python manage.py cc_backup_score')
+#         # os.system('python manage.py cc_backup_score')
 
-@receiver(post_save, sender=Midi)
-def backup_midi_on_upload(sender, instance, **kwargs):
-    """Backup midi"""
+# @receiver(post_save, sender=Midi)
+# def backup_midi_on_upload(sender, instance, **kwargs):
+#     """Backup midi"""
 
-    if os.environ['DJANGO_SETTINGS_MODULE'] == 'choralcentral.settings.prod':
-        os.system('python manage.py cc_backup_midi')
+#     if os.environ['DJANGO_SETTINGS_MODULE'] == 'choralcentral.settings.prod':
+#         os.system('python manage.py cc_backup_midi')
