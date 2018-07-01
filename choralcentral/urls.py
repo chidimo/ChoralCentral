79e82348100774db0e9ffe8d57aeab6f96de95fb
orji chidi <orjichidi95@gmail.com>
2018-07-01 03:14:15 +0000

    fix error in main urls file

--- choralcentral/urls.py
+++ choralcentral/urls.py
@@ -70,11 +70,9 @@ urlpatterns += [
     path('terms-of-use/', views.terms_of_use),
 ]
 
-urlpatterns += [
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
