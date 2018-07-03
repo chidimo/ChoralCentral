2ce52c4c09a5d114d99bbacf583fff75b1d432ba
orji chidi <orjichidi95@gmail.com>
2018-07-03 23:25:32 +0000

    add facebook properties to song

--- scheduled_tasks/utils.py
+++ scheduled_tasks/utils.py
@@ -1,11 +1,19 @@
-# `source virtualenvwrapper.sh && workon __cc && python utils.py`
-
+# /home/parousia/.virtualenvs/choralcentral-8n7QT1vQ/bin/python /home/parousia/choralcentral/scheduled_tasks/utils.py
 import os
-import shutil
 
-from django.conf import settings
+cmd1 = 'python manage.py algolia_reindex'
+cmd2 = 'python manage.py cc_backup_score'
+cmd3 = 'python manage.py cc_backup_midi'
 
-tmp = os.path.join(settings.BASE_DIR, 'media', 'tmp')
+os.system(cmd1) # reindex algolia database
+os.system(cmd2) # reindex algolia database
+os.system(cmd3) # reindex algolia database
 
-shutil.rmtree(tmp) # remove tmp directory
-os.system('python manage.py algolia_reindex') # reindex algolia database
+with open('cmds_.txt', 'w+') as f:
+    f.write('Executed commands\n\n')
+    f.write(cmd1)
+    f.write('\n')
+    f.write(cmd2)
+    f.write('\n')
+    f.write(cmd3)
+    f.write('\n')
